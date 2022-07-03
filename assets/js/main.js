cd0137d88e08cd5b8632099a2b0b91e830d1c4e7
Dmytro Rudenko <romantikp71@Dmytros-MacBook-Pro.local>
2022-07-03 19:29:51 +0300

    first version

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+ }
+
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
